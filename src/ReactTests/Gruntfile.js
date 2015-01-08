5358bc5dfaaa877c769f2317b9a41d96f978b1b1
Van Kichline <vkichline@hotmail.com>
2015-01-07 20:43:06 -0800

    Initial checkin
    One of the React JSX demos, set up for a demo.

---
+++ src/ReactTests/Gruntfile.js
@@ -0,0 +1,16 @@
+﻿/// <binding BeforeBuild='build' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        react: {
+            files: {
+                expand: true,
+                cwd: 'jsx',
+                src: ['**/*.jsx'],
+                dest: 'wwwroot/scripts',
+                ext: '.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-react");
+    grunt.registerTask('build', ['react']);
+};
