efd44932683c3c759af835e0db550b30fb2837a3
Van Kichline <vkichline@hotmail.com>
2015-04-28 10:50:05 -0700

    Upgraded to RC

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ReactTests
 Demo of simple Dev14 React project frome React homesite with mods
 
-Built with Visual Studio 2015 CTP5
+Built with Visual Studio 2015 RC
--- src/ReactTests/Startup.cs
+++ src/ReactTests/Startup.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
 
 namespace ReactTests
 {
     public class Startup
     {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         }
     }
 }
